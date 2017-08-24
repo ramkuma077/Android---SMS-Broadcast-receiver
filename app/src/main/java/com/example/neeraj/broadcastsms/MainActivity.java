a6e67bee423b9194e51ca66dacce347537138a3a
ramkuma077 <ramkuma077@gmail.com>
2017-08-24 13:42:38 +0530

    push the code

---
+++ app/src/main/java/com/example/neeraj/broadcastsms/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.neeraj.broadcastsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button1);
+        b2=(Button)findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "service started", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "stop services", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/neeraj/broadcastsms/SmsReceiver.java
@@ -0,0 +1,38 @@
+package com.example.neeraj.broadcastsms;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+/**
+ * Created by neeraj on 16-04-2017.
+ */
+
+public class SmsReceiver extends BroadcastReceiver {
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String TAG = "SMSBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent.getAction();
+
+        if (intent.getAction().equals(SMS_RECEIVED)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                final SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+
+                }
+                if (messages.length > -1) {
+                   /* ((TAG) "MessageRecieved:" + messages[0].getMessageBody()); */
+                }
+
+            }
+        }
+    }
+}
